a74018c1a883afee79d16d4a8ada677b6f894d57
ndkhanh207 <0306231207@caothang.edu.vn>
2026-02-23 14:11:35 +0700

    update: field validation

--- src/main/java/com/cinema/seatbooking/entity/User.java
+++ src/main/java/com/cinema/seatbooking/entity/User.java
@@ -2,6 +2,9 @@ package com.cinema.seatbooking.entity;
 
 import com.cinema.seatbooking.common.UserRole;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +22,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Email(message = "Must be a valid email")
+    @NotNull(message = "Email is required")
     private String email;
 
     @Column(nullable = false)
+    @Min(value = 10, message = "Password must be at least 10 characters")
     private String password;
 
     @Column(nullable = false, unique = true)
